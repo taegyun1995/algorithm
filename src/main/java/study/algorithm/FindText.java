82b53770f87f20d8b99071402216f9ee45a19bbc
gyunny <needle12s@gmail.com>
2023-01-19 20:55:31 +0900

    1. find text

---
+++ src/main/java/study/algorithm/FindText.java
@@ -0,0 +1,27 @@
+package study.algorithm;
+
+import java.util.Scanner;
+
+public class FindText {
+    public int solution(String str, char t) {
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) == t) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FindText T = new FindText();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+        System.out.println(T.solution(str, c));
+    }
+}
