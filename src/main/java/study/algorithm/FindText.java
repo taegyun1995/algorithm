3e9b06bab126fc45be5d12abc220b980fbe91e7e
gyunny <needle12s@gmail.com>
2023-01-19 22:19:18 +0900

    wip

--- src/main/java/study/algorithm/FindText.java
+++ src/main/java/study/algorithm/FindText.java
@@ -8,12 +8,15 @@ public class FindText {
         str = str.toUpperCase();
         t = Character.toUpperCase(t);
 
-        for (int i = 0; i < str.length(); i++) {
-
-            if (str.charAt(i) == t) {
-                answer++;
-            }
+        for (char x : str.toCharArray()) {
+            if(x == t) answer++;
         }
+
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.charAt(i) == t) {
+//                answer++;
+//            }
+//        }
         return answer;
     }
 
