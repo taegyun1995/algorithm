5abb88a771c5dd8f6284b5b6821723dfdb327be8
gyunny <needle12s@gmail.com>
2023-01-24 20:45:04 +0900

    Extract Only Numbers

--- src/main/java/study/algorithm/ExtractOnlyNumbers.java
+++ src/main/java/study/algorithm/ExtractOnlyNumbers.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ExtractOnlyNumbers {
 
-    public  int solution(String s) {
+    public int solution(String s) {
 //        int answer = 0;
 //        for (char x : s.toCharArray()) {
 //            if (x >= 48 && x <= 57) answer = answer * 10 + (x - 48);
