50c5de4cea01ded93016d9b92ae9e65adf963de9
gyunny <needle12s@gmail.com>
2023-01-25 20:12:38 +0900

    Encrypt

---
+++ src/main/java/study/algorithm/Encrypt.java
@@ -0,0 +1,28 @@
+package study.algorithm;
+
+import java.util.Scanner;
+
+public class Encrypt {
+
+    public String solution(int n, String s) {
+        String answer = "";
+        for (int i = 0; i < n; i++) {
+            String tmp = s.substring(0, 7).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2);
+            answer += (char) num;
+            s = s.substring(7);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Encrypt T = new Encrypt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+
+        System.out.print(T.solution(n, s));
+    }
+
+}
