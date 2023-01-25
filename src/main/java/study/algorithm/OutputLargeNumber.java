f41b529182470d0d32c4c64e7ce846329542c8dc
gyunny <needle12s@gmail.com>
2023-01-25 16:09:21 +0900

    Output Large Number

---
+++ src/main/java/study/algorithm/OutputLargeNumber.java
@@ -0,0 +1,33 @@
+package study.algorithm;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class OutputLargeNumber {
+
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i - 1]) {
+                answer.add(arr[i]);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        OutputLargeNumber T = new OutputLargeNumber();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+
+}
