464ca5acedb6441bb7a52cf31f5a89beed6d7480
gyunny <needle12s@gmail.com>
2023-01-27 19:30:32 +0900

    Visiable Student

--- src/main/java/study/inf/Array/VisibleStudent.java
+++ src/main/java/study/inf/Array/VisibleStudent.java
@@ -5,15 +5,25 @@ import java.util.Scanner;
 public class VisibleStudent {
 
     public int solution(int n, int[] arr) {
-        int count = 0;
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            if (max < arr[i]) {
-                count++;
+//        int count = 0;
+//        int max = Integer.MIN_VALUE;
+//        for (int i = 0; i < n; i++) {
+//            if (max < arr[i]) {
+//                count++;
+//                max = arr[i];
+//            }
+//        }
+//        return count;
+
+        int answer = 1;
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                answer++;
                 max = arr[i];
             }
         }
-        return count;
+        return answer;
     }
 
     public static void main(String[] args) {
