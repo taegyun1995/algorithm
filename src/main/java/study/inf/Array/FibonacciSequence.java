3e0dbd35739128187d1d340cd59d6cf5b4d0aa9d
gyunny <needle12s@gmail.com>
2023-01-27 21:20:48 +0900

    Fibonacci Sequence

--- src/main/java/study/inf/Array/FibonacciSequence.java
+++ src/main/java/study/inf/Array/FibonacciSequence.java
@@ -4,23 +4,43 @@ import java.util.Scanner;
 
 public class FibonacciSequence {
 
-    public int[] solution(int n) {
-        int[] answer = new int[n];
-        answer[0] = 1;
-        answer[1] = 1;
-        for (int i = 1; i < n - 1; i++) {
-            answer[i + 1] = answer[i] + answer[i - 1];
-        }
+//    public int[] solution(int n) {
+//        int[] answer = new int[n];
+//        answer[0] = 1;
+//        answer[1] = 1;
+//        for (int i = 2; i < n; i++) {
+//            answer[i] = answer[i - 2] + answer[i - 1];
+//        }
+//
+//        return answer;
+//    }
 
-        return answer;
+//    public static void main(String[] args) {
+//        FibonacciSequence T = new FibonacciSequence();
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for (int x : T.solution(n)) {
+//            System.out.print(x + " ");
+//        }
+//    }
+
+    public void solution(int n) {
+        int a = 1;
+        int b = 1;
+        int c;
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
     }
 
     public static void main(String[] args) {
         FibonacciSequence T = new FibonacciSequence();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int x : T.solution(n)) {
-            System.out.print(x + " ");
-        }
+        T.solution(n);
     }
 }
