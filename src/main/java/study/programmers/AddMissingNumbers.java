1d69389472ec1d80330dfbbc97be432b65d4772a
gyunny <needle12s@gmail.com>
2023-01-28 20:33:48 +0900

    Add Missing Numbers

---
+++ src/main/java/study/programmers/AddMissingNumbers.java
@@ -0,0 +1,30 @@
+package study.programmers;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AddMissingNumbers {
+
+    public int solution(int n, int[] numbers) {
+        int result = 45;
+
+        int sum = Arrays.stream(numbers).sum();
+        result = result - sum;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        AddMissingNumbers T = new AddMissingNumbers();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        System.out.println(T.solution(n, numbers));
+    }
+
+
+}
