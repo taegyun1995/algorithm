8bbf4e9e002bcf00e5eddf44848a570dc71ac9df
gyunny <needle12s@gmail.com>
2023-01-31 19:32:32 +0900

    Number Of Factors And Addition

---
+++ src/main/java/study/programmers/NumberOfFactorsAndAddition.java
@@ -0,0 +1,46 @@
+package study.programmers;
+
+import java.util.Scanner;
+
+public class NumberOfFactorsAndAddition {
+
+    public static void main(String[] args) {
+        NumberOfFactorsAndAddition T = new NumberOfFactorsAndAddition();
+        Scanner sc = new Scanner(System.in);
+        int left = sc.nextInt();
+        int right = sc.nextInt();
+        System.out.println(T.solution(left, right));
+    }
+
+    public int solution(int left, int right) {
+//        int answer = 0;
+//
+//        for (int i = left; i <= right; i++) {
+//            int count = 0;
+//            for (int j = 1; j <= i; j++) {
+//                if (i % j == 0) {
+//                    count++;
+//                }
+//            }
+//            if (count % 2 == 0) {
+//                answer += i;
+//            } else {
+//                answer -= i;
+//            }
+//        }
+//        return answer;
+
+        int answer = 0;
+
+        for (int i = left; i <= right; i++) {
+            if (i % Math.sqrt(i) == 0) {
+                answer -= i;
+            } else {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+
+}
