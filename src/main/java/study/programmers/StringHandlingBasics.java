cb16a745a76e10ad5b5738ae53af3b93ef135e5a
gyunny <needle12s@gmail.com>
2023-01-31 20:28:06 +0900

    String Handling Basics

---
+++ src/main/java/study/programmers/StringHandlingBasics.java
@@ -0,0 +1,35 @@
+package study.programmers;
+
+import java.util.Scanner;
+
+public class StringHandlingBasics {
+
+    public static void main(String[] args) {
+        StringHandlingBasics T = new StringHandlingBasics();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(T.solution(s));
+    }
+
+    public boolean solution(String s) {
+//        char tmp;
+//        if (s.length() == 4 || s.length() == 6) {
+//            for (int i = 0; i < s.length(); i++) {
+//                tmp = s.charAt(i);
+//                if (!Character.isDigit(tmp)) {
+//                    return false;
+//                }
+//            }
+//        } else {
+//            return false;
+//        }
+//
+//        return true;
+
+        if ((s.length() == 4) || (s.length() == 6)) {
+            return s.matches("(^[0-9]*$)");
+        }
+        return false;
+    }
+
+}
