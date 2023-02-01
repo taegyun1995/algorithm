bbc8327a93e29090439731336df9cbce02e822ba
gyunny <needle12s@gmail.com>
2023-02-01 13:10:02 +0900

    Matrix Addition

---
+++ src/main/java/study/programmers/MatrixAddition.java
@@ -0,0 +1,34 @@
+package study.programmers;
+
+import java.util.Scanner;
+
+public class MatrixAddition {
+
+    public static void main(String[] args) {
+        MatrixAddition T = new MatrixAddition();
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int column = sc.nextInt();
+        int[][] arr1 = new int[row][column];
+        int[][] arr2 = new int[row][column];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                arr1[i][j] =sc.nextInt();
+                arr2[i][j] =sc.nextInt();
+            }
+        }
+
+        System.out.println(T.solution(arr1, arr2));
+    }
+
+    public int[][] solution(int[][]arr1, int[][]arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for(int i = 0; i < arr1.length; i++){
+            for(int j = 0; j < arr1[i].length; j++){
+                answer[i][j]=arr1[i][j]+arr2[i][j];
+            }
+        }
+        return answer;
+    }
+
+}
