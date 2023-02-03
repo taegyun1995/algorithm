bdafcbd2da423f77af5f1061c7d4847f86d9992d
gyunny <needle12s@gmail.com>
2023-02-03 20:27:55 +0900

    Find Text

--- src/main/java/study/inflearn/String/FindText.java
+++ src/main/java/study/inflearn/String/FindText.java
@@ -7,18 +7,15 @@ public class FindText {
     public static void main(String[] args) {
         FindText t = new FindText();
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
+        String str = sc.next();
         char c = sc.next().charAt(0);
-        System.out.println(t.solution(s, c));
+        System.out.println(t.solution(str, c));
     }
 
-    public int solution(String s, char c) {
+    public int solution(String str, char c) {
         int answer = 0;
-        s = s.toUpperCase();
-        c = Character.toUpperCase(c);
-        char[] chars = s.toCharArray();
-        for (char aChar : chars) {
-            if (aChar == c) {
+        for (char aChar : str.toUpperCase().toCharArray()) {
+            if (aChar == Character.toUpperCase(c)) {
                 answer++;
             }
         }
