1694d8c2d12fe1cc23bd4fe7c2a737cd9cec7421
gyunny <needle12s@gmail.com>
2023-02-04 22:56:02 +0900

    Flip Specifit Words

---
+++ src/main/java/study/inflearn/String/FlipSpecificWords.java
@@ -0,0 +1,37 @@
+package study.inflearn.String;
+
+import java.util.Scanner;
+
+public class FlipSpecificWords {
+
+    public static void main(String[] args) {
+        FlipSpecificWords t = new FlipSpecificWords();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(t.solution(s));
+    }
+
+    public String solution(String s) {
+        String answer = "";
+        char[] chars = s.toCharArray();
+        int lt = 0;
+        int rt = 0;
+        while (lt < rt) {
+            if (!Character.isAlphabetic(chars[lt])) {
+                lt++;
+            } else if (!Character.isAlphabetic(chars[rt])) {
+                rt--;
+            } else {
+                char tmp = chars[lt];
+                chars[lt] = chars[rt];
+                chars[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+        answer = s;
+
+        return answer;
+    }
+
+}
