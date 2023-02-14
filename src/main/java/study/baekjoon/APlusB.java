ec71da5c563f40050ea5c07e8361d7c4b993a444
gyunny <needle12s@gmail.com>
2023-02-14 17:04:47 +0900

    A PlUS B

--- src/main/java/study/baekjoon/APlusB.java
+++ src/main/java/study/baekjoon/APlusB.java
@@ -1,2 +1,19 @@
-package study.baekjoon;public class APlusB {
+package study.baekjoon;
+
+import java.util.Scanner;
+
+public class APlusB {
+
+    public static void main(String[] args) {
+        APlusB t = new APlusB();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(t.solution(a, b));
+    }
+
+    public int solution(int a, int b) {
+        return a + b;
+    }
+
 }
