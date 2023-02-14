47e63daa900bf0ad731a8ad0c541913786a19074
gyunny <needle12s@gmail.com>
2023-02-14 17:09:57 +0900

    A Multiply B

---
+++ src/main/java/study/baekjoon/AMultiplyB.java
@@ -0,0 +1,19 @@
+package study.baekjoon;
+
+import java.util.Scanner;
+
+public class AMultiplyB {
+
+    public static void main(String[] args) {
+        AMultiplyB t = new AMultiplyB();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(t.solution(a, b));
+    }
+
+    public int solution(int a, int b) {
+        return a * b;
+    }
+
+}
