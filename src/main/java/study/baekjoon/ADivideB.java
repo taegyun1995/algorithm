059e1ef01868ed1415242813278a68666aa4f590
gyunny <needle12s@gmail.com>
2023-02-14 17:17:00 +0900

    A Divide B

---
+++ src/main/java/study/baekjoon/ADivideB.java
@@ -0,0 +1,19 @@
+package study.baekjoon;
+
+import java.util.Scanner;
+
+public class ADivideB {
+
+    public static void main(String[] args) {
+        ADivideB t = new ADivideB();
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println(t.solution(a, b));
+    }
+
+    public double solution(double a, double b) {
+        return a / b;
+    }
+
+}
