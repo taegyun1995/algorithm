e4ab6fc1ac757a615c03ee47e10d1a1ed208be82
gyunny <needle12s@gmail.com>
2023-02-14 17:06:59 +0900

    A MINUS B

---
+++ src/main/java/study/baekjoon/AMinusB.java
@@ -0,0 +1,19 @@
+package study.baekjoon;
+
+import java.util.Scanner;
+
+public class AMinusB {
+
+    public static void main(String[] args) {
+        AMinusB t = new AMinusB();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(t.solution(a, b));
+    }
+
+    public int solution(int a, int b) {
+        return a - b;
+    }
+
+}
