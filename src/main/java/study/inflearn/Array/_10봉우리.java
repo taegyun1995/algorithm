f2ea6529f54a19ecba6ebf4c0b41db1aa5c46e8f
gyunny <needle12s@gmail.com>
2023-02-17 14:58:15 +0900

    정수 내림차순으로 배치

---
+++ src/main/java/study/inflearn/Array/_10봉우리.java
@@ -0,0 +1,45 @@
+package study.inflearn.Array;
+
+import java.util.Scanner;
+
+public class _10봉우리 {
+
+    public static void main(String[] args) {
+        _10봉우리 t = new _10봉우리();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(t.solution(n, arr));
+    }
+
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+
+    public int solution(int n, int[][] arr) {
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (nx >= 0 && nx < n && ny >=0 && ny < n && arr[nx][ny] >= arr[i][j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+}
