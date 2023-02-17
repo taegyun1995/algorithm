d82e94eca87f390bf080d995bee71259fc069225
gyunny <needle12s@gmail.com>
2023-02-17 12:06:38 +0900

    격자판 최대합

---
+++ src/main/java/study/inflearn/Array/_9격자판최대합.java
@@ -0,0 +1,46 @@
+package study.inflearn.Array;
+
+import java.util.Scanner;
+
+public class _9격자판최대합 {
+
+    public static void main(String[] args) {
+        _9격자판최대합 t = new _9격자판최대합();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(t.solution(n, arr));
+    }
+
+    public int solution(int n, int[][] arr) {
+        int answer = Integer.MIN_VALUE;
+        int sum1;
+        int sum2;
+        for (int i = 0; i < n; i++) {
+            sum1 = 0;
+            sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+        sum1 = 0;
+        sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+
+        return answer;
+    }
+
+}
