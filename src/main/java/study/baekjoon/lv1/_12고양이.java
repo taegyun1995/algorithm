bc43dbfc0ea3deff2eb612f7dfab39917839e86f
gyunny <needle12s@gmail.com>
2023-02-22 11:04:50 +0900

    고양이

---
+++ src/main/java/study/baekjoon/lv1/_12고양이.java
@@ -0,0 +1,18 @@
+package study.baekjoon.lv1;
+
+public class _12고양이 {
+
+    public static void main(String[] args) {
+        String s1 = "\\    /\\";
+        String s2 = " )  ( ')";
+        String s3 = "(  /  )";
+        String s4 = " \\(__)|";
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+    }
+
+
+}
