bc00cad97d093bd6fb13be203f6472766e8d6173
gyunny <needle12s@gmail.com>
2023-02-22 11:06:13 +0900

    개

---
+++ src/main/java/study/baekjoon/lv1/_13개.java
@@ -0,0 +1,19 @@
+package study.baekjoon.lv1;
+
+public class _13개 {
+
+    public static void main(String[] args) {
+        String s1 = "|\\_/|";
+        String s2 = "|q p|   /}";
+        String s3 = "( 0 )\"\"\"\\";
+        String s4 = "|\"^\"`    |";
+        String s5 = "||_/=\\\\__|";
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+    }
+
+}
