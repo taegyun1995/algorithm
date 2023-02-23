c3e82399759656893cdcc3de7845324d4e5040ec
gyunny <needle12s@gmail.com>
2023-02-23 21:05:37 +0900

    가위 바위 보

--- src/main/java/study/inflearn/Array/_03가위바위보.java
+++ src/main/java/study/inflearn/Array/_03가위바위보.java
@@ -1,11 +1,12 @@
 package study.inflearn.Array;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ScissorsRockPaper {
+public class _03가위바위보 {
 
     public static void main(String[] args) {
-        ScissorsRockPaper T = new ScissorsRockPaper();
+        _03가위바위보 T = new _03가위바위보();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] a = new int[n];
@@ -21,15 +22,15 @@ public class ScissorsRockPaper {
         }
     }
 
-    public char[] solution(int n, int[] a, int[] b) {
-        char[] answer = new char[n];
+    public ArrayList<Character> solution(int n, int[] a, int[] b) {
+        ArrayList<Character> answer = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (a[i] == 1 && b[i] == 3 || a[i] == 2 && b[i] == 1 || a[i] == 3 && b[i] == 2) {
-                answer[i] = 'A';
+            if ((a[i] == 1 && b[i] == 3) || (a[i] == 2 && b[i] == 1) || (a[i] == 3 && b[i] == 2)) {
+                answer.add('A');
             } else if (a[i] == b[i]) {
-                answer[i] = 'D';
+                answer.add('D');
             } else {
-                answer[i] = 'B';
+                answer.add('B');
             }
         }
 
