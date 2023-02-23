15bebb899decb7b446d5b54474d58a8b86a696b7
gyunny <needle12s@gmail.com>
2023-02-23 19:19:35 +0900

    임시 반장 정하기

---
+++ src/main/java/study/inflearn/Array/_11임시반장정하기.java
@@ -0,0 +1,43 @@
+package study.inflearn.Array;
+
+import java.util.Scanner;
+
+public class _11임시반장정하기 {
+
+    public static void main(String[] args) {
+        _11임시반장정하기 t = new _11임시반장정하기();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n + 1][6];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 5; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(t.solution(n, arr));
+    }
+
+    public int solution(int n, int[][] arr) {
+        int answer = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if (cnt > max) {
+                max = cnt;
+                answer = i;
+            }
+        }
+        return answer;
+    }
+
+}
