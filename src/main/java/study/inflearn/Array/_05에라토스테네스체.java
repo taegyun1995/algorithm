303be45e2a1d97b6bde479ffaa193afacd2c5401
gyunny <needle12s@gmail.com>
2023-02-23 21:58:51 +0900

    소수 에라토스테네스 체

--- src/main/java/study/inflearn/Array/_05에라토스테네스체.java
+++ src/main/java/study/inflearn/Array/_05에라토스테네스체.java
@@ -2,16 +2,33 @@ package study.inflearn.Array;
 
 import java.util.Scanner;
 
-public class EratosthenesSieve {
+public class _05에라토스테네스체 {
 
     public static void main(String[] args) {
-        EratosthenesSieve T = new EratosthenesSieve();
+        _05에라토스테네스체 T = new _05에라토스테네스체();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(T.solution(n));
     }
 
     public int solution(int n) {
+        int tmp = 0;
+        for (int i = 2; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    cnt++;
+                }
+            }
+            if (cnt == 2) {
+                tmp++;
+            }
+        }
+
+        return tmp;
+    }
+
+    public int solution2(int n) {
         int answer = 0;
         int[] ch = new int[n + 1];
         for (int i = 2; i <= n; i++) {
@@ -22,6 +39,7 @@ public class EratosthenesSieve {
                 }
             }
         }
+
         return answer;
     }
 
