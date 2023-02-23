38eb756f9bd1604c539e4edbb19a101dcd6a5c81
gyunny <needle12s@gmail.com>
2023-02-23 21:15:23 +0900

    피보나치 수열

--- src/main/java/study/inflearn/Array/_04피보나치수열.java
+++ src/main/java/study/inflearn/Array/_04피보나치수열.java
@@ -2,10 +2,10 @@ package study.inflearn.Array;
 
 import java.util.Scanner;
 
-public class FibonacciSequence {
+public class _04피보나치수열 {
 
     public static void main(String[] args) {
-        FibonacciSequence t = new FibonacciSequence();
+        _04피보나치수열 t = new _04피보나치수열();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int x : t.solution(n)) {
@@ -24,17 +24,15 @@ public class FibonacciSequence {
         return arr;
     }
 
-    public void solution2(int n) {
-        int a = 1;
-        int b = 1;
-        int c;
-        System.out.print(a + " " + b + " ");
+    public int[] solution2(int n) {
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 1;
         for (int i = 2; i < n; i++) {
-            c = a + b;
-            System.out.print(c + " ");
-            a = b;
-            b = c;
+            arr[i] = arr[i - 2] + arr[i - 1];
         }
+
+        return arr;
     }
 
 }
