8354929d74fd9a6185fca898d62ab377f0cee42c
gyunny <needle12s@gmail.com>
2023-02-28 14:01:25 +0900

    A + B - 4

---
+++ src/main/java/study/baekjoon/lv3/_12APlusBMinus4.java
@@ -0,0 +1,18 @@
+package study.baekjoon.lv3;
+
+import java.util.Scanner;
+
+public class _12APlusBMinus4 {
+
+    public static void main(String[] args) {
+        _12APlusBMinus4 t = new _12APlusBMinus4();
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+
+}
