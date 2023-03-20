63c3519fc0a1651bfa87830da82bafe70912f480
gyunny <needle12s@gmail.com>
2023-03-20 15:57:28 +0900

    암호

--- src/main/java/study/inflearn/String/_12암호.java
+++ src/main/java/study/inflearn/String/_12암호.java
@@ -2,10 +2,10 @@ package study.inflearn.String;
 
 import java.util.Scanner;
 
-public class Encrypt {
+public class _12암호 {
 
     public static void main(String[] args) {
-        Encrypt t = new Encrypt();
+        _12암호 t = new _12암호();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String s = sc.next();
@@ -13,16 +13,15 @@ public class Encrypt {
     }
 
     public String solution(int n, String s) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             String tmp = s.substring(0, 7).replace('#', '1').replace('*', '0');
             int num = Integer.parseInt(tmp, 2);
-            answer += (char) num;
+            sb.append((char)num);
             s = s.substring(7);
         }
 
-
-        return answer;
+        return sb.toString();
     }
 
 }
