d57bf7068a55f0df40146ed6ba531d3d60411283
gyunny <needle12s@gmail.com>
2023-03-20 15:55:32 +0900

    문자열 압축

--- src/main/java/study/inflearn/String/_11문자열압축.java
+++ src/main/java/study/inflearn/String/_11문자열압축.java
@@ -2,32 +2,32 @@ package study.inflearn.String;
 
 import java.util.Scanner;
 
-public class StringCompression {
+public class _11문자열압축 {
 
     public static void main(String[] args) {
-        StringCompression t = new StringCompression();
+        _11문자열압축 t = new _11문자열압축();
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         System.out.println(t.solution(str));
     }
 
     public String solution(String s) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         s = s + " ";
         int cnt = 1;
         for (int i = 0; i < s.length() - 1; i++) {
             if (s.charAt(i) == s.charAt(i + 1)) {
                 cnt++;
             } else {
-                answer += s.charAt(i);
+                sb.append(s.charAt(i));
                 if (cnt > 1) {
-                    answer += String.valueOf(cnt);
+                    sb.append(cnt);
                     cnt = 1;
                 }
             }
         }
 
-        return answer;
+        return sb.toString();
     }
 
 }
