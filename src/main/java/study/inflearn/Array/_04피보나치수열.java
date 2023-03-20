604f1b41a79e8bf8a06f5e179acf15aa377d3919
gyunny <needle12s@gmail.com>
2023-03-20 16:10:04 +0900

    피보나치 수열

--- src/main/java/study/inflearn/Array/_04피보나치수열.java
+++ src/main/java/study/inflearn/Array/_04피보나치수열.java
@@ -13,18 +13,8 @@ public class _04피보나치수열 {
         }
     }
 
+    // 다이나믹 프로그래밍 -> 입력 값에 따라 계산 시간이 선형적으로 증가, 시간 복잡도 = O(n)
     public int[] solution(int n) {
-        int[] arr = new int[n];
-        arr[0] = 1;
-        arr[1] = 1;
-        for (int i = 2; i < n; i++) {
-            arr[i] = arr[i - 2] + arr[i - 1];
-        }
-
-        return arr;
-    }
-
-    public int[] solution2(int n) {
         int[] arr = new int[n];
         arr[0] = 1;
         arr[1] = 1;
