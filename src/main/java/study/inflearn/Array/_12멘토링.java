898f24e080dad1d7f67325babe448b39640dc442
gyunny <needle12s@gmail.com>
2023-03-21 22:17:58 +0900

    멘토링

---
+++ src/main/java/study/inflearn/Array/_12멘토링.java
@@ -0,0 +1,49 @@
+package study.inflearn.Array;
+
+import java.util.Scanner;
+
+public class _12멘토링 {
+
+    public static void main(String[] args) {
+        _12멘토링 t = new _12멘토링();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println(t.solution(n, m, arr));
+    }
+
+    public int solution(int n, int m, int[][] arr) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int cnt = 0;
+                for (int k = 0; k < m; k++) {
+                    int pi = 0, pj = 0;
+                    for (int s = 0; s < n; s++) {
+                        if (arr[k][s] == i) {
+                            pi = s;
+                        }
+                        if (arr[k][s] == j) {
+                            pj = s;
+                        }
+                    }
+                    if (pi < pj) {
+                        cnt++;
+                    }
+                }
+                if (cnt == m) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+}
