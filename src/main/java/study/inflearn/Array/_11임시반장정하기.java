8e4fe1a153bb916c311a72a0a05efaf15aa59171
gyunny <needle12s@gmail.com>
2023-03-21 21:04:09 +0900

    임시 반장 정하기

--- src/main/java/study/inflearn/Array/_11임시반장정하기.java
+++ src/main/java/study/inflearn/Array/_11임시반장정하기.java
@@ -24,6 +24,7 @@ public class _11임시반장정하기 {
 
         for (int i = 1; i <= n; i++) {
             int cnt = 0;
+
             for (int j = 1; j <= n; j++) {
                 for (int k = 1; k <= 5; k++) {
                     if (arr[i][k] == arr[j][k]) {
@@ -32,11 +33,13 @@ public class _11임시반장정하기 {
                     }
                 }
             }
+
             if (cnt > max) {
                 max = cnt;
                 answer = i;
             }
         }
+
         return answer;
     }
 
