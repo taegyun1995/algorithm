63673c14eb5711185b5877f74abb304aff3ef6d3
taegyun1995 <needle12s@gmail.com>
2023-04-15 22:51:32 +0900

    쇠막대기

---
+++ src/main/java/study/inflearn/StackQueue/_05쇠막대기.java
@@ -0,0 +1,24 @@
+package study.inflearn.StackQueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _05쇠막대기 {
+
+    public static void main(String[] args) throws IOException {
+        _05쇠막대기 t = new _05쇠막대기();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.print(t.solution(s));
+    }
+
+
+    public int solution(String s) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+}
