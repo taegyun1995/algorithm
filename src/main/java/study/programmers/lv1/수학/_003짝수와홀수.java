9bbd9eebd0236e14ef67362427d5489c96224f3c
taegyun1995 <needle12s@gmail.com>
2023-09-29 17:53:47 +0900

    수학/짝수와홀수

--- src/main/java/study/programmers/lv1/수학/_003짝수와홀수.java
+++ src/main/java/study/programmers/lv1/수학/_003짝수와홀수.java
@@ -6,8 +6,8 @@ public class _003짝수와홀수 {
 
     public static void main(String[] args) throws IOException {
         _003짝수와홀수 t = new _003짝수와홀수();
-        int n = 3;
-        int n2 = 4;
+        int n = 4;
+        int n2 = 3;
         System.out.println(t.solution(n));
         System.out.print(t.solution(n2));
     }
